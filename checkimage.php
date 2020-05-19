7183ebeaef5453729c60eadb420ae4331287ac16
Yohann MaiVan <yohanm888@gmail.com>
2020-05-19 18:46:33 +0200

    [ADD] script js get picture

---
+++ checkimage.php
@@ -0,0 +1,19 @@
+<?php
+$img = $_POST['data'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$fileData = base64_decode($img);
+
+$fileName = 'photo.png';
+$f = file_put_contents($fileName, $fileData);
+
+
+if ($f) print 1;
+else print 0;
+
+$data = $img;
+
+//header('Content-Type: application/json');
+
+echo $data;
+?>
