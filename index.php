26daed016463d6c2b7f6e730cc44a8f48c76e922
Maris <maris@macbook-pro-de-maris.home>
2020-06-02 20:15:31 +0200

    fdsfsdf

--- index.php
+++ index.php
@@ -9,44 +9,55 @@ require "merge.php";?>
     <link rel="stylesheet" href="css/index.css">
     <title>connecté</title>
 </head>
-<body class='z'>
+<body class='z site'>
         <div class='z header'>
+                <div class="header__name">
                 <?php
                 if (isset($_SESSION['login']))
                 {
-                        echo "Bonjour, " .$_SESSION['login'];
-                ?>
-        	<a href="profil.php" id="profile"><span>Mon profil</span></a>
-                <a href="deconnexion.php">Se déconnecter</a> <?php
+                       ?> <p class="square_btn"><?php echo "Bonjour  " .$_SESSION['login'];?></p>
+                &nbsp; &nbsp;
+                <?php
                 }
-                ?>
+                ?></div>
+                <div class="header__menu">
+                <a href="profil.php" id="profile" class="square_btn">Mon profil</a>&nbsp;
+                <a href="deconnexion.php" class="square_btn">Se déconnecter</a>&nbsp;
+                <a href="deconnexion.php" class="square_btn">Accueil</a>&nbsp;
+                <a href="deconnexion.php" class="square_btn">Gallerie</a>&nbsp;
+                </div>
         </div>
         <div class="z section">
-            <div class="separator"></div>
-            <div class="section-menu">
-                <div class="z section-menu__cam">
-                    <div ><video id="video" width="400px" height="400px" autoplay></video></div>
-                    <div><button id="snap" disabled>Snap Photo</button></div>
+            <div class="z center">
+                <img src="img/logo2.png" width="100px" height="100px">
+            </div>
+            <div class="z section-menu">
+                <div class="section-left">
+                        <div class="z filter">
+                                    <div>
+                                    <input type="radio" id="filter1" name="filtre">
+                                    <img src="img/leo.png" alt="leo" height="70" width="70">
+                                    </div>
+                                    <div>
+                                    <input type="radio" id="filter2" name="filtre">
+                                    <img src="img/leo.png" alt="leo" height="70" width="70">
+                                    </div>
+                                    <div>
+                                    <input type="radio" id="filter3" name="filtre">
+                                    <img src="img/leo.png" alt="leo" height="70" width="70">
+                                    </div>
+                                </div>    
+                    <div class="z section-menu__cam">
+                        <div><video id="video" width="300px" height="300px" autoplay></video></div>
+                        <div><button id="snap" disabled>Snap Photo</button></div>
+                    </div>
                 </div>
                 <div class="z section-menu-images"> 
                     <div class="z canvas">
-                        <canvas id="canvas" width="600px" height="450px"></canvas>
-                    </div>    
-                    <div class="z filter">
-                        <div>
-                        <input type="radio" id="filter1" name="filtre">
-                        <img src="img/leo.png" alt="leo" height="100" width="100">
-                        </div>
-                        <div>
-                        <input type="radio" id="filter1" name="filtre">
-                        <img src="img/leo.png" alt="leo" height="100" width="100">
-                        </div>
+                        <canvas id="canvas" width="300px" height="200px"></canvas>
                     </div>    
                 </div>
              </div>
-             <?php
-print("LOL");
-?>
         </div>
         <div class="z footer">
             <div>Camagru</div>
